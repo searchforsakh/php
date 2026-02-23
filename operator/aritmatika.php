8eb1dc5a0cb7ed317e215efd5f04d74d9fc0bb1c
searchforsakh <skhpratama13@gmail.com>
2026-02-23 17:45:13 +0700

    latihan aritmatika

---
+++ operator/aritmatika.php
@@ -0,0 +1,18 @@
+<?php
+
+$angka1 = 3;
+$angka2 = 5;
+
+$hasil = $angka1 + $angka2;
+echo $hasil;
+echo "\n";
+
+$hasilnegativ = -$hasil;
+echo $hasilnegativ;
+echo "\n";
+
+$hasilmodulo = $hasil % 3;
+echo $hasilmodulo;
+echo "\n";
+
+echo "hasil perkalian ", $angka1, " * ", $angka2, " = ", $angka1*$angka2;
