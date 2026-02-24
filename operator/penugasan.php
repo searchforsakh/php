7c5e3d3d4155c9d5d1b5cb91b3ee4dd2f5baa4f1
searchforsakh <skhpratama13@gmail.com>
2026-02-24 17:11:47 +0700

    aritmatika penugasan

---
+++ operator/penugasan.php
@@ -0,0 +1,17 @@
+<?php
+
+$hasil = 0;
+$angka1 = 3;
+$angka2 = 5;
+$angka3 = 7;
+
+$hasil += $angka1;
+$hasil += $angka2;
+$hasil += $angka2;
+$hasil += $angka3;
+
+// $hasil = $angka1 + $angka2;
+// $hasil = $hasil + $angka2;
+// $hasil = $hasil + $angka3;
+
+echo $hasil;
