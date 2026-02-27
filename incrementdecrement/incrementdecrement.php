5f42b74de3682ce1e47d7cef166737ab77a8ef1c
searchforsakh <skhpratama13@gmail.com>
2026-02-27 17:00:15 +0700

    increment decrement

---
+++ incrementdecrement/incrementdecrement.php
@@ -0,0 +1,36 @@
+<?php
+
+// menaikkan dan menurunkan satu data
+
+// post increment
+$angka1 = 125;
+$angka1++;
+echo $angka1;
+echo "\n";
+
+$angka1--;
+echo $angka1;
+echo "\n";
+--$angka1;
+echo $angka1;
+echo "\n";
+
+// $angka1 = 125; // tidak akan dijumlahkan, karena php menampilkan terlebih dahulu, baru menjumlahkan
+// echo $angka1++;
+
+// pre increment
+$angka1 = 125;
+echo ++$angka1;
+echo "\n";
+echo --$angka1;
+echo "\n";
+echo --$angka1;
+
+
+
+
+
+
+
+
+
