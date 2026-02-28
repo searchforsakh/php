b63d39845b0ca5531763a84cf7d50a37f4558a6a
searchforsakh <skhpratama13@gmail.com>
2026-03-01 00:15:34 +0700

    update array

--- operator/operator array/operatorarray.php
+++ operator/operator array/operatorarray.php
@@ -1,1 +1,15 @@
 <?php
+
+$first = [
+    "firstname" => "sakha"
+    ];
+    
+    $last = [
+    "firstname" => "sakha", // php tidak menampilkan key array yang sama dengan yg diatasnya
+    "nama pertama" => "budi",
+    "lastname" => "pratama"
+];
+
+$full = $first+$last;
+var_dump($full);
+// echo $full; // echo tidak bisa menampilkan array, hanya bisa string dan angka.
